b9c966a839ca8d64d9ecbad8c6fa6f65784c8505
crocogene <crocogene@yandex.com>
2024-05-05 16:53:41 +0300

    cryptsetup

--- mkfs.sh
+++ mkfs.sh
@@ -13,20 +13,25 @@ mkfs.vfat -n "$efi_name" -F 32 "$efi_partition"
 # Tank
 tank_disk="/dev/mapper/$crypt_name"
 if [[ -f "$tank_disk" ]]; then
-  cryptsetup close "$tank_disk"
+  cryptsetup close "$tank_disk"&
+  wait
 fi
 wipefs -a "$tank_partition"
 cryptsetup luksFormat --type=luks2 "$tank_partition" --verbose --batch-mode
-cryptsetup open "$tank_partition" "$crypt_name"
+cryptsetup open "$tank_partition" "$crypt_name" --batch-mode
 mkfs.btrfs --force -L "$tank_label" "$tank_disk"
 export BTRFS_OPT=compress-force=zstd:1,noatime,discard=async,commit=120
 mount -o "$BTRFS_OPT" "$tank_disk" /mnt
-btrfs subvolume create /mnt/{@,@home,@snapshots}
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@snapshots
 umount /mnt
 mount -o "$BTRFS_OPT",subvol=@ "$tank_disk" /mnt
 mkdir /mnt/{home,var}
 mount -o "$BTRFS_OPT",subvol=@home "$tank_disk" /mnt/home/
-btrfs subvolume create /mnt/var/{cache,log,tmp}
+btrfs subvolume create /mnt/var/cache
+btrfs subvolume create /mnt/var/log
+btrfs subvolume create /mnt/var/tmp
 
 # Swap
 
