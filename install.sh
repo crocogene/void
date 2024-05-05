631904a54f63937a89b35b67378db9e8d971f7e9
crocogene <crocogene@yandex.com>
2024-05-05 23:31:40 +0300

    install

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/env bash
 
-repo=https://repo-default.voidlinux.org/current/musl
-arch=x86_64-musl
-arch_glibc=x86_64
+repo=https://repo-default.voidlinux.org/current
+arch=x86_64
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -12,7 +11,7 @@ cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
 mkdir /mnt/etc
 cp -r "$SCRIPT_DIR"/etc/. /mnt/etc/
 
-XBPS_ARCH=$arch xbps-install -Suvy -r /mnt -R "$repo" base-system pam-mount opendoas pigz micro moar iwd
+XBPS_ARCH="$arch"-musl xbps-install -Suvy -r /mnt -R "$repo"/musl base-system pam-mount opendoas pigz micro moar iwd
 
 cd /mnt/usr/local ; rm -rf sbin ; ln -s bin sbin
 cd /mnt/usr/bin ; ln -s pigz gzip ; ln -s unpigz gunzip
@@ -24,9 +23,12 @@ ln -s ../etc etc
 ln -s ../proc proc
 ln -s ../sys sys
 ln -s ../var/log var/log
-XBPS_ARCH=$arch_glibc xbps-install -Suvy -r /mnt/glibc -R "$repo" -S base-voidstrap
+XBPS_ARCH="$arch" xbps-install -Suvy -r /mnt/glibc -R "$repo" -S base-voidstrap
 
 cd /mnt/glibc/usr/local ; rm -rf sbin ; ln -s bin sbin
 
 cp "$SCRIPT_DIR"/chroot.sh /mnt/root/
-xchroot /mnt /bin/bash /root/chroot.sh
+xchroot /mnt /bin/bash 
+
+umount --recursive --force /mnt
+
