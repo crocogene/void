39faa1b450cced8d908057c83862cad9c528e6c4
crocogene <crocogene@yandex.com>
2024-05-05 19:49:20 +0300

    install script

--- install.sh
+++ install.sh
@@ -7,9 +7,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 mkdir -p /mnt/var/db/xbps/keys
 cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
-XBPS_ARCH=$ARCH xbps-install -S -y -r /mnt -R "$REPO" base-system opendoas pigz moar micro iwd
+mkdir /mnt/etc
 cp -r "$SCRIPT_DIR"/etc/. /mnt/etc/
-cp "$SCRIPT_DIR"/chroot.sh /mnt/root/
+XBPS_ARCH=$ARCH xbps-install -S -y -r /mnt -R "$REPO" base-system opendoas pigz micro moar iwd
+#xbps-remove -y -r /mnt sudo gzip nvi less wpa_supplicant
+#xbps-remove -y -r /mnt linux-firmware-{broadcom,intel}
 cd /mnt/usr/local ; rm -rf sbin ; ln -s bin sbin
 cd /mnt/usr/bin ; ln -s pigz gzip ; ln -s unpigz gunzip
-xchroot /mnt /bin/bash /root/chroot.sh
+
+cp "$SCRIPT_DIR"/chroot.sh /mnt/root/
+BTRFS_OPT="$BTRFS_OPT" PS1="$PS1" xchroot /mnt /bin/bash /root/chroot.sh
