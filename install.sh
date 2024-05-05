32e624161255f90aafab56f6871f5f065f99ce6b
crocogene <crocogene@yandex.com>
2024-05-06 00:15:18 +0300

    install

--- install.sh
+++ install.sh
@@ -11,19 +11,25 @@ cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
 mkdir /mnt/etc
 cp -r "$SCRIPT_DIR"/etc/. /mnt/etc/
 
-XBPS_ARCH="$arch"-musl xbps-install -Suvy -r /mnt -R "$repo"/musl base-system pam-mount opendoas pigz micro moar iwd
+XBPS_ARCH="$arch"-musl xbps-install -Suvy -r /mnt -R "$repo"/musl -S \
+  base-system pam-mount opendoas pigz micro moar iwd \
+  turnstile seatd socklog-void \
+  mesa-dri 
 
 cd /mnt/usr/local ; rm -rf sbin ; ln -s bin sbin
 cd /mnt/usr/bin ; ln -s pigz gzip ; ln -s unpigz gunzip
 
 mkdir -p /mnt/glibc/var
 cd /mnt/glibc
-ln -s ../dev dev
-ln -s ../etc etc
-ln -s ../proc proc
-ln -s ../sys sys
-ln -s ../var/log var/log
-XBPS_ARCH="$arch" xbps-install -Suvy -r /mnt/glibc -R "$repo" -S base-voidstrap
+ln -s /dev dev
+ln -s /etc etc
+ln -s /proc proc
+ln -s /sys sys
+ln -s /var/log var/log
+ln -s /boot boot
+XBPS_ARCH="$arch" xbps-install -Suvy -r /mnt/glibc -R "$repo" -S \
+#  base-container \
+  nvidia
 
 cd /mnt/glibc/usr/local ; rm -rf sbin ; ln -s bin sbin
 
