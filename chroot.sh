8a1d52f9bc4d1285f993f4de6bf72c129a61c2c1
crocogene <crocogene@yandex.com>
2024-05-05 19:38:34 +0300

    chroot

--- chroot.sh
+++ chroot.sh
@@ -1,13 +1,7 @@
 #!/bin/env bash
 
-xbps-remove -y sudo gzip nvi less wpa_supplicant linux-firmware-broadcom linux-firmware-intel
+#xbps-remove -y sudo gzip nvi less wpa_supplicant linux-firmware-broadcom linux-firmware-intel
 
-cd /usr/local
-rm -rf sbin
-ln -s bin sbin
 
-cd /usr/bin
-ln -s pigz gzip
-ln -s unpigz gunzip
 
-/bin/bash
+PS1="[chroot]$PS1" /bin/bash
--- install.sh
+++ install.sh
@@ -10,4 +10,6 @@ cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
 XBPS_ARCH=$ARCH xbps-install -S -y -r /mnt -R "$REPO" base-system opendoas pigz moar micro iwd
 cp -r "$SCRIPT_DIR"/etc/. /mnt/etc/
 cp "$SCRIPT_DIR"/chroot.sh /mnt/root/
+cd /mnt/usr/local ; rm -rf sbin ; ln -s bin sbin
+cd /mnt/usr/bin ; ln -s pigz gzip ; ln -s unpigz gunzip
 xchroot /mnt /bin/bash /root/chroot.sh
