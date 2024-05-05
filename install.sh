3a36128d6fd122308c5d0f92c3dec84bf28e2d84
crocogene <crocogene@yandex.com>
2024-05-05 19:22:16 +0300

    chroot

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
 XBPS_ARCH=$ARCH xbps-install -S -y -r /mnt -R "$REPO" base-system opendoas pigz moar micro iwd
 cp -r "$SCRIPT_DIR"/etc/. /mnt/etc/
 cp "$SCRIPT_DIR"/chroot.sh /mnt/root/
-BTRFS_OPT="$BTRFS_OPT" xchroot /mnt /bin/bash /root/void/chroot.sh
+xchroot /mnt /bin/bash /root/chroot.sh
