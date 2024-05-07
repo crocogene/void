cac527f327e9e2b1b7e5dbf82b0e5168e80b8872
crocogene <crocogene@yandex.com>
2024-05-07 19:39:08 +0300

    mountfs

--- mountfs.sh
+++ mountfs.sh
@@ -17,18 +17,24 @@ function error {
 }
 
 if findmnt /mnt &>/dev/null; then
-  error "Already mounted"
+  echo "Already mounted\n"
+else
+  tank_disk=/dev/mapper/$crypt_name
+  if [[ ! -L $tank_disk ]]; then
+    cryptsetup open $tank_partition $crypt_name --verbose --batch-mode ||
+      error "Can't open crypted tank"
+  fi  
+  mount -o $btrfs_opt,subvol=@ $tank_disk /mnt ||
+    error "Can't mount subvol @"
+  mount -o $btrfs_opt,subvol=@home $tank_disk /mnt/home/ ||
+    error "Can't mount subvol @home"
+  mount -o noatime $efi_partition /mnt/boot/efi ||
+    error "Can't mount efi partition"
 fi
-tank_disk=/dev/mapper/$crypt_name
-if [[ ! -L $tank_disk ]]; then
-  cryptsetup open $tank_partition $crypt_name --verbose --batch-mode ||
-    error "Can't open crypted tank"
-fi  
-mount -o $btrfs_opt,subvol=@ $tank_disk /mnt ||
-  error "Can't mount subvol @"
-mount -o $btrfs_opt,subvol=@home $tank_disk /mnt/home/ ||
-  error "Can't mount subvol @home"
-mount -o noatime $efi_partition /mnt/boot/efi ||
-  error "Can't mount efi partition"
+if [ $1 eq "wipe" ]; then
+  rm -rf /mnt/* &>/dev/null
+  echo "Wiped!\n"
+fi
+
 
 
